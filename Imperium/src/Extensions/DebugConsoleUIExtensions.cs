015f68180d1f0019581616f5e3f9dd1b01456021
Zehs <andrey5764zehsteam@gmail.com>
2026-01-05 04:13:16 -0600

    Removed unused class extension (DebugConsoleUIExtensions.cs)

--- Imperium/src/Extensions/DebugConsoleUIExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Imperium.Extensions;
-
-public static class DebugConsoleUIExtensions
-{
-    public static bool IsOpen(this DebugConsoleUI debugConsoleUI) => debugConsoleUI.chatActive;
-}
